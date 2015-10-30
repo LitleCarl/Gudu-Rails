de6f3c1f7684cb4d23c41729a0f1aac850d9fe9e
LitleCarl <cjx5813@foxmail.com>
2015-10-30 13:35:06 +0800

    给商品添加月售

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151028141344) do
+ActiveRecord::Schema.define(version: 20151030053249) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "name",            limit: 255
@@ -148,6 +148,7 @@ ActiveRecord::Schema.define(version: 20151028141344) do
     t.string   "pinyin",        limit: 255
     t.datetime "created_at",                                 null: false
     t.datetime "updated_at",                                 null: false
+    t.integer  "month_sale",    limit: 4,   default: 23
   end
 
   create_table "specifications", force: :cascade do |t|

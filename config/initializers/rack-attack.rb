6e95bd2c6a3ac5a036dbb6d6c54cac8dde786b69
= <=>
2016-03-08 14:18:30 +0800

    完善ip限制

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -27,9 +27,9 @@ class Rack::Attack
   # Throttle all requests by IP (60rpm)
   #
   # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
-  # throttle('req/ip', :limit => 100, :period => 1.minutes) do |req|
-  #   req.ip unless req.path.start_with?('/assets')
-  # end
+  throttle('req/ip', :limit => 130, :period => 1.minutes) do |req|
+    req.ip unless req.path.start_with?('/assets')
+  end
 
   ### Prevent Brute-Force Login Attacks ###
 

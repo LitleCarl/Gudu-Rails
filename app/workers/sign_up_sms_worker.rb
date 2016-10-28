f8368b0e6805a97c70ef13286de2b13be5f261d5
Apple <apple@Mac-mini.local>
2016-10-28 21:00:10 +0800

    验证码发送

--- app/workers/sign_up_sms_worker.rb
+++ app/workers/sign_up_sms_worker.rb
@@ -2,7 +2,7 @@ class SignUpSmsWorker
   include Sidekiq::Worker
 
   def perform(phone, code)
-    template_params = {code: code, product: '早餐巴士'}
+    template_params = {code: code.to_s, product: '早餐巴士'}
     Sms.sms_alidayu(phone, Sms::TemplateID::ALI_LoginCode, template_params)
   end
 end
